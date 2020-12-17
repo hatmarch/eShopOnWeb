7b2f2f2a4c669e70596851d2c71da83169a179b6
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2020-12-17 01:10:51 +0000

    Changes for demo

--- deploy/oc-db-seed.sh
+++ deploy/oc-db-seed.sh
@@ -2,6 +2,27 @@
 
 set -euxo pipefail
 
-oc whoami
-oc whoami --show-server
-oc projects
+# take in argument from pipeline/command line
+declare PROJECT=$1
+echo "Seeding database on project $PROJECT"
+
+declare DATABASE_SVC="hplus-db"
+
+# FIXME: Uncomment to access the oc command
+# oc project $PROJECT
+# oc port-forward svc/${DATABASE_SVC} 1433:1433 &
+
+# connection settings for the seeding
+export ConnectionStrings__CatalogConnection='Server=localhost; UID=sa; Password=yourStrong(!)Password; Integrated Security=False; Initial Catalog=Microsoft.eShopOnWeb.CatalogDb;'
+export ConnectionStrings__IdentityConnection='Server=localhost; UID=sa; Password=yourStrong(!)Password; Integrated Security=False; Initial Catalog=Microsoft.eShopOnWeb.Identity;'
+export ASPNETCORE_ENVIRONMENT="Development"
+export ASPNETCORE_URLS="http://*:8080"
+
+cd src/Web
+
+dotnet tool restore
+dotnet ef database update -c catalogcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
+dotnet ef database update -c appidentitydbcontext -p ../Infrastructure/Infrastructure.csproj -s Web.csproj
+
+echo "Database $DATABASE_SVC in namespace $PROJECT successfully initialized."
+
